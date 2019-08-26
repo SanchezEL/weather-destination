f7df687d526f176e3e53523824202b1e416af83f
Emilio Sanchez <sanchez.emilio483@gmail.com>
2019-08-26 10:43:51 -0500

    added the ability to logout

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,34 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Nav() {
-  return (
-    <div className="nav">
-      <h1>Weather Destination</h1>
-    </div>
-  )
+// export default function Nav() {
+//   return (
+//     <div className="nav">
+//       <h1>Weather Destination</h1>
+//     </div>
+//   )
+// }
+import React, { Component } from 'react'
+import { Button } from '@material-ui/core'
+
+
+export default class Nav extends Component {
+
+  logout = (e) => {
+    e.preventDefault()
+    document.cookie = 'id_token= ;expires=Thu, 01 Jan 1970 00:00:01 GMT'
+    window.location.reload();
+  }
+  
+  render() {
+    return (
+      <div className="nav">
+        <div className="title">
+          <h1>Weather Destination</h1>
+        </div>
+        <div className="logoutButton">
+          <Button onClick={this.logout}>Logout</Button>
+        </div>
+      </div>
+    )
+  }
 }
